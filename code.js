018646d331a9c986cdca55032b396b7241ce99d5
AyeshaZahid <ayeshazahid758@gmail.com>
2021-04-17 02:29:23 +0000

    2hourComplete

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
-
+             // Hour 01   DAY#01
+/*
 console.log("hello world!");
 //way to write comments
 //alert("yoo!");
@@ -36,7 +37,7 @@ console.log(num1);
 
 /*working with functions
 After making a function it is necessary to call it.
-*/
+
 // creating function
 function fun(){
     alert("Hey there!");
@@ -100,7 +101,7 @@ console.log(fruits.split(""));// splits every single character
 console.log(fruits.split(",")); //splits each word by commas
  
 //working with arrays
-//let fruit= ["apple","banana","pineapple","blueberry"];
+let fruit= ["apple","banana","pineapple","blueberry"];
 //console.log(fruit[2]);
 let fruit= new Array("apple","banana","pineapple","blueberry");
 console.log(fruit);
@@ -113,4 +114,99 @@ for (let i=0; i< fruit.length;  i++){
 console.log(fruit.toString());   // converting array into string
 console.log(fruit.join("+"));    // using join
 console.log( fruit.pop() + fruit );  // removes last item
+//ALSO shift() can be used for the same purpose 
+console.log(fruit.shift());
+console.log(fruit);
 console.log(fruit.push("blackberries"), fruit); //appends items
+
+
+            // Hour 2 (DAY #02)
+            let fruit= ["apple","banana","pineapple","blueberry"];
+            let vegetables = ["carrot","tomato","onions","cabbage","capscicum"];
+            console.log(fruit.concat(vegetables));    // concatenation
+            let allVeg = fruit.concat(vegetables);
+            console.log(allVeg.sort());
+
+            let Numbers = [1,6,8,9,0,4,67,334,567,890];
+            console.log(Numbers.sort(function(a,b){return (a-b)}));   // for assending order 
+            console.log(Numbers.sort(function arr(a,b){return b-a}));     // for descending order  // both are perfectly okay
+            // pushing numbers in an empty array
+            let emptyArr = new Array();
+            for (let num=0; num <10; num++) {
+             emptyArr.push(num);
+             
+            }
+            console.log(emptyArr);
+
+// working with OBJECTS (called Dictonaries in PYTHON)
+// don't initate an object with let
+var myObject = {
+    first: 'Ayesha',
+    last:'Zahid',
+    age: 21,
+    height:'5.6',
+};
+myObject.age++;
+console.log(myObject.age);
+console.log(myObject.first);
+
+// Conditionals , Control flows, (if else statements)
+// let dont work with prompt
+// Make a simple  program to select 18-50 as your targetted demographic audience
+var age = prompt("Enter your age please ....");
+if ( (age>=18) && (age<=50)){
+    status = "Targetted audience"
+    console.log(status);
+}
+    else{
+        status= "Not my concern";
+        console.log(status);
+    }
+    */
+
+// using SWITCH CASE
+//print weekend for fridays , saturdays & sundays
+//print weekday for rest of the days
+
+//( How can we code in a way that whether user give put n 
+//upper case or lowercase it will any how respond correctly)
+var answer = prompt ("Name the Day?");
+switch(answer){
+    case "friday":
+        text = "Weekend";
+        break;
+    case "saturday":
+        text= "Weekend";
+        break;
+    case "sunday":
+        text= "Weekend";
+        break;
+    default:
+        text = "Weekday"
+
+}
+console.log(text);
+
+// Making Json file
+//Json is valid JavaScript
+
+let dump= `[
+    {
+
+
+    "first" : "Ayesha",
+    "last" :"Zahid",
+    "age" : 21,
+    "height" :5.6
+},
+{
+    "first": "wali",
+    "last" :"Zahid",
+    "age" : 18,
+    "height" :5.8
+        
+    }
+    
+]`
+//console.log(JSON.parse(dump)[0].first);
+console.log(JSON.parse(dump));
