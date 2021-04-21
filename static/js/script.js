1122e8f37b28885efbbb05570f9f6c11eec0885f
AyeshaZahid <ayeshazahid758@gmail.com>
2021-04-21 21:35:42 +0000

    StepIntoFunctionalProgramming

--- static/js/script.js
+++ static/js/script.js
@@ -12,9 +12,46 @@ document.getElementById("flex-box-result").appendChild(h1);
 function reset(){
      document.getElementById("flex-box-result").remove();
 };
+// getting image on click
 function generateCat(){
     var image = document.createElement("img");
     var div = document.getElementById("flex-cat-gen");
     image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small"
     div.appendChild(image);
 }
+
+//Challenge #03 Rock, Paper, Scissors
+/// learning Functional Programming
+  // referring the object
+
+function rpsGame(yourChoice){
+    console.log(yourChoice.src);
+var HumanChoice, BotChoice;
+//HumanChoice = yourChoice.id;
+BotChoice=numberToChoice(randTorpsInt());
+console.log(BotChoice);
+/*var results;
+
+//var message;
+//function finalMessage(results);  //will return an object
+//rpsFrontEnd(yourChoice.id,BotChoice.id,message);
+*/
+}
+ 
+ 
+ 
+ function randTorpsInt(){
+     return Math.floor(Math.random()*3);
+ }
+ function numberToChoice(number){
+    return ["rock","paper","scissors"][number];
+ }
+ function decideWinner(yourChoice,CompChoice){
+     var rpsDatabase ={
+         "rock": {"scissors":0.5,"paper":0,"rock":0.5},
+         "paper":{"paper":0.5 , "rock":0 , "scissors":1},
+         "scissors":{"scissors":0.5 , "paper":1 , "rock":0}
+
+     
+     
+ }
